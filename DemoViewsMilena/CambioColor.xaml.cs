b99e6b8908964665905629dad716ccade46d12f6
goingtop <sebasipi501@gmail.com>
2018-08-09 17:02:55 -0500

    color

--- DemoViewsMilena/CambioColor.xaml.cs
+++ DemoViewsMilena/CambioColor.xaml.cs
@@ -11,5 +11,22 @@ namespace DemoViewsMilena
         {
             InitializeComponent();
         }
+        void CambiarColor(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var rojo = slrojo.Value;
+            var azul = slazul.Value;
+            var verde = slverde.Value;
+            Color bgcolor = new Color(rojo, verde, azul);
+            boxcolor.BackgroundColor = bgcolor;
+            lbDisplay.Text = ColorToHex(bgcolor);
+        }
+        private string ColorToHex(Color color){
+            int rojo = (int)(color.R * 255);
+            int verde = (int)(color.G * 255);
+            int azul = (int)(color.B * 255);
+            int alpha = (int)(color.A * 255);
+            return $"#{rojo:X2}{verde:X2}{azul:X2}{alpha:X2}";
+        }
+
     }
 }
